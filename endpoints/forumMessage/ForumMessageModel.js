6113bc255d141e79a64fde716fcfe668ca45cd11
Elrond <saCul@gmx.li>
2022-05-22 23:11:09 +0200

    Abgabe MS2 (praktisch nichts...)

--- endpoints/forumMessage/ForumMessageModel.js
+++ endpoints/forumMessage/ForumMessageModel.js
@@ -1,12 +1,1 @@
 const mongoose = require('mongoose');
-
-const ForumThreadSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  ownerID: String
-}, { timestamps: true}
-);
-
-const ForumThread = mongoose.model('ForumThread', ForumThreadSchema);
-
-module.exports = ForumThread;
