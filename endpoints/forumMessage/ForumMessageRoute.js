bcb180bf10fce4c9e79c4374c1f0ebd790eac9f1
Elrond <saCul@gmx.li>
2022-06-05 07:49:22 +0200

    ...

---
+++ endpoints/forumMessage/ForumMessageRoute.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const express = require("express")
+const router = express.Router()
+const logger = require("../../config/winston")
+const ForumMessage = require("./ForumMessageModel")
+const FMService = require("./ForumMessageService")
+const { isAuth, isAdmin } = require("../authentication/AuthenticationService")
+
+
+/* create ForumMessage */
+router.post("/", isAuth, async (req, res, next) => {
+  logger.debug(`Creating new ForumMessage... `)
+  try {
+    const ForumMessage = await FMService.createFM(req.body, req.user.userID)
+    res.status(201).send(ForumMessage)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+module.exports = router
